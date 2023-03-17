bd7e0fcc519d4e17150dea1143f7850ae65a25da
Kuspsu <joosepkrassavin@gmail.com>
2023-03-17 10:40:35 +0200

    Refactored effective address decoding to it's own function

--- 01-sim8086/src/mov.c
+++ 01-sim8086/src/mov.c
@@ -9,7 +9,6 @@ int ins6disp(int byte, FILE* fp, char* ins) {
 	const char* r_m;
 	const char* reg;
 	char eac_str[24]; // String for displacement storing
-	int dis_16 = 0;
 
 	// D field check, if REG field is destination
 	int d = byte & 2;
@@ -21,19 +20,31 @@ int ins6disp(int byte, FILE* fp, char* ins) {
 		return 2; // Missing opcode's additional data bytes
 	}
 
+	int failure = decode_effective_address(&reg, &r_m, eac_str, byte, fp, w);
+	if (failure)
+		return failure;
+	else if (d)
+		printf("%s %s, %s\n", ins, reg, r_m);
+	else
+		printf("%s %s, %s\n", ins, r_m, reg);
+
+	return 0;
+};
+
+int decode_effective_address(const char** reg, const char** r_m, char* eac_str, int byte, FILE* fp, int w) {
+	int dis_16 = 0;
+
 	switch(byte & 0b11000000) {
 		case 0b11000000: // Register to register
-			reg = field_decode[((byte & 0b00111000) >> 3) + w];
-			r_m = field_decode[(byte & 0b00000111) + w];
-			printf("%s %s, %s\n", ins, r_m, reg);
-			return 0;
-
+			*reg = field_decode[((byte & 0b00111000) >> 3) + w];
+			*r_m = field_decode[(byte & 0b00000111) + w];
 		break;
+
 		case 0b10000000: // 16-bit displacement
 			dis_16 = 1;
 			__attribute__ ((fallthrough)); // For compiler: switch case fall through is desirable
 		case 0b01000000: // 8-bit displacement
-			reg = field_decode[((byte & 0b00111000) >> 3) + w];
+			*reg = field_decode[((byte & 0b00111000) >> 3) + w];
 			int r_m_field = (byte & 0b00000111);
 			int val = 0;
 
@@ -53,9 +64,9 @@ int ins6disp(int byte, FILE* fp, char* ins) {
 
 			if (val) {
 				sprintf(eac_str, ea_calc_dir[r_m_field], val);
-				r_m = eac_str;
+				*r_m = eac_str;
 			} else {
-				r_m = ea_calc[r_m_field];
+				*r_m = ea_calc[r_m_field];
 			}
 			break;
 
@@ -66,17 +77,12 @@ int ins6disp(int byte, FILE* fp, char* ins) {
 				return 4;
 			}
 
-			r_m = ea_calc[(byte & 0b00000111)];
-			reg = field_decode[((byte & 0b00111000) >> 3) + w];
+			*r_m = ea_calc[(byte & 0b00000111)];
+			*reg = field_decode[((byte & 0b00111000) >> 3) + w];
 	}
 
-	if (d)
-		printf("%s %s, %s\n", ins, reg, r_m);
-	else
-		printf("%s %s, %s\n", ins, r_m, reg);
-
 	return 0;
-};
+}
 
 int movIM_REG(int byte, FILE* fp) {
 	const char* reg;
--- 01-sim8086/src/mov.h
+++ 01-sim8086/src/mov.h
@@ -5,6 +5,8 @@
 
 int ins6disp(int data, FILE* fp, char* ins);
 
+int decode_effective_address(const char** reg, const char** r_m, char* eac_str, int byte, FILE* fp, int w);
+
 int movIM_REG(int byte, FILE* fp);
 
 #endif
