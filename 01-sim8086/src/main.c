134db4504e786a695f481a8306878ac4ae5fa8ea
Joosep Krassavin <Joosep.Krassavin@websystems.ee>
2023-03-28 08:14:28 +0300

    Moved includes out of main.h

--- 01-sim8086/src/main.c
+++ 01-sim8086/src/main.c
@@ -1,5 +1,9 @@
 
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// #include "main.h"
+#include "opcode.h"
 #include "debug.h"
 
 int main(int argc, char const *argv[])
--- 01-sim8086/src/main.h
+++ 01-sim8086/src/main.h
@@ -1,9 +1,4 @@
 #ifndef _sim8086__main
 #define _sim8086__main 1
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "opcode.h"
-
 #endif
