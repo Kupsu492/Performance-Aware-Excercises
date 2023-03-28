eb7b612729c4a9cce62168c95c0c123764c031db
Joosep Krassavin <Joosep.Krassavin@websystems.ee>
2023-03-28 08:42:09 +0300

    Moved struct definition to .h file

--- 01-sim8086/src/reg.h
+++ 01-sim8086/src/reg.h
@@ -1,6 +1,28 @@
 #ifndef _sim8086__reg
 #define _sim8086__reg 1
 
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct instruction
+{
+	uint8_t operation;
+
+	// Will contain registers or effective address calculation encodings
+	uint8_t op1;
+	uint8_t op2;
+
+	int32_t data; // Immediate value
+	int32_t disp; // EAC constant value
+
+	bool wide;
+	bool sign;
+	bool dir;
+	uint8_t size;
+
+	int32_t error; // If processing failed
+} instruction;
+
 /*	Table for register name decoding
 	First 8 are 8bit registers
 	Second 8 are 16bit registers */
