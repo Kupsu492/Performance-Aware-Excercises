269da36e5720532391ab2ef240f792bd75b0998b
Kuspsu <joosepkrassavin@gmail.com>
2023-06-14 20:04:23 +0300

    03: Added argument and file opening

--- 03-haversine-input-generator/src/main.c
+++ 03-haversine-input-generator/src/main.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
     char file_name[30];
+    int length;
+    FILE *fPtr = NULL;
 
-    // Try to create new data file
-    for (int i = 1; i < 15; ++i)
-    {
-         sprintf(file_name, "data-%02d-input.json", i);
-         printf("%s\n", file_name);
+    if (argc < 2) {
+        printf("Missing line count argument");
+        return -1;
     }
 
+    length = atoi(argv[1]);
+    if (length < 1) {
+        printf("Length either below 1 or not a number");
+        return -1;
+    }
 
-    // if (access("./data.json", F_OK) == 0) {
-    //     // file exists
-    // } else {
-    //     // file doesn't exist
-    // }
+    // Try to create a new data file
+    for (int i = 1; i < 99; ++i)
+    {
+        sprintf(file_name, "./data-%02d-input.json", i);
+
+        // For testing
+        fPtr = fopen(file_name, "w");
+        break;
+
+        if (access(file_name, F_OK) != 0) {
+            fPtr = fopen(file_name, "w");
+            break;
+        }
+    }
+    if (fPtr == NULL) {
+        printf("json file couldn't be created: %s", file_name);
+        return -1;
+    }
+
 
 
     return 0;
