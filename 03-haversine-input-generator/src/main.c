57767dc9d010cf8ac01e724bded493fb1673c726
Kuspsu <joosepkrassavin@gmail.com>
2024-01-13 09:13:48 +0200

    03: Point generation

--- 03-haversine-input-generator/src/main.c
+++ 03-haversine-input-generator/src/main.c
@@ -2,11 +2,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdint.h>
+
+void generate_points(FILE *fPtr, int length) {
+    double max = 180.0;
+    double x1, x2, y1, y2;
+
+    fprintf(fPtr,"{\"pairs\":[");
+    int i = length - 1;
+    while (1) {
+        x1 = ((double)rand()/(double)(RAND_MAX)) * max;
+        x2 = ((double)rand()/(double)(RAND_MAX)) * max;
+        y1 = ((double)rand()/(double)(RAND_MAX)) * max;
+        y2 = ((double)rand()/(double)(RAND_MAX)) * max;
+        
+        if (i == 0) break;
+
+        fprintf(fPtr,"{\"x0\":%lf, \"y0\":%lf, \"x1\":%lf, \"y1\":%lf},", x1, y1, x2, y2);
+        i--;
+    }
+    fprintf(fPtr,"{\"x0\":%lf, \"y0\":%lf, \"x1\":%lf, \"y1\":%lf}]}", x1, y1, x2, y2);
+}
 
 int main(int argc, char const *argv[]) {
     char file_name[30];
     int length;
-    int seed;
+    int64_t seed;
     FILE *fPtr = NULL;
 
     if (argc >= 3) {
@@ -14,6 +35,7 @@ int main(int argc, char const *argv[]) {
     } else {
         seed = time(NULL);
     }
+    srand(seed);
 
     if (argc < 2) {
         printf("Missing line count argument");
@@ -45,7 +67,10 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
 
-    printf("Creating json file with %d lines and using seed: %d\n", length, seed);
+    printf("Creating json file with %d lines and using seed: %ld\n", length, seed);
 
+    generate_points(fPtr, length);
+
+    fclose(fPtr);
     return 0;
 }
