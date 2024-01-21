9acc09b8675e9de87cdba57556fc332d5c578f8e
Kuspsu <joosepkrassavin@gmail.com>
2024-01-21 14:00:20 +0200

    01: Fix indentation

--- 01-sim8086/src/decode.c
+++ 01-sim8086/src/decode.c
@@ -4,43 +4,43 @@
 #include "reg.h"
 
 int32_t check_opcode(stream *file_stream, instruction *instruction) {
-    uint8_t opcode = *(file_stream->data + file_stream->pos);
+	uint8_t opcode = *(file_stream->data + file_stream->pos);
 
-    // 6bit opcodes
-    switch(opcode & 0b11111100) {
-        case 0b00000000:
-            // return ins6disp(opcode, fp, "add\0");
-        case 0b10000000:
-            // return ins_disp_data(opcode, fp);
-        case 0b10001000:
-            instruction->op = OP_MOV;
-            return ins6disp(file_stream, instruction);
-        case 0b00101000:
-            // return ins6disp(opcode, fp, "sub\0");
-        case 0b00111000:
-            // return ins6disp(opcode, fp, "cmp\0");
-        case 0b11100000:
-            // return jump_decode(opcode, fp, 1);
-        break;
-    }
+	// 6bit opcodes
+	switch(opcode & 0b11111100) {
+		case 0b00000000:
+			// return ins6disp(opcode, fp, "add\0");
+		case 0b10000000:
+			// return ins_disp_data(opcode, fp);
+		case 0b10001000:
+			instruction->op = OP_MOV;
+			return ins6disp(file_stream, instruction);
+		case 0b00101000:
+			// return ins6disp(opcode, fp, "sub\0");
+		case 0b00111000:
+			// return ins6disp(opcode, fp, "cmp\0");
+		case 0b11100000:
+			// return jump_decode(opcode, fp, 1);
+		break;
+	}
 
-    // 4bit opcodes
-    switch(opcode & 0b11110000) {
-        case 0b10110000:
-            // return movIM_REG(opcode, fp);
-        case 0b01110000:
-            // return jump_decode(opcode, fp, 0);
-        break;
-    }
+	// 4bit opcodes
+	switch(opcode & 0b11110000) {
+		case 0b10110000:
+			// return movIM_REG(opcode, fp);
+		case 0b01110000:
+			// return jump_decode(opcode, fp, 0);
+		break;
+	}
 
-    // Immediate to accumulator opcodes
-    switch(opcode & 0b11000110) {
-        case 0b00000100:
-            // return decode_immediate_accumulator(opcode, fp);
-        break;
-    }
+	// Immediate to accumulator opcodes
+	switch(opcode & 0b11000110) {
+		case 0b00000100:
+			// return decode_immediate_accumulator(opcode, fp);
+		break;
+	}
 
-    return 1;
+	return 1;
 }
 
 /*
